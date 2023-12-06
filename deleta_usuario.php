c666d368c0338c565759bbaab692e9d027817ad3
Gabriel Pereira <152038489+gabrielPereira360@users.noreply.github.com>
2023-12-06 09:17:00 -0300

    Add files via upload

--- deleta_usuario.php
+++ deleta_usuario.php
@@ -12,18 +12,28 @@ $matricula = $_SESSION['matricula'];
 
 include "conexao.php";
 
+$sql = "SELECT * FROM `usuarios` where id_usuario = $id";
 
-$sql = "DELETE FROM `usuarios` WHERE id_usuario = $id;";
+$buscar = mysqli_query($conn, $sql);
+while ($array = mysqli_fetch_array($buscar)) {
+  $matriculausuario = $array['matricula_usuario'];
+}
+
+if ($matricula == $matriculausuario) {
+  print "<script>alert('Não é possível excluir um administrador por aqui!');</script>";
+  print "<script>location.href='?page=home';</script>";
+} else {
+  $sql = "DELETE FROM `usuarios` WHERE id_usuario = $id;";
 
 
-$res = mysqli_query($conn, $sql);
+  $res = mysqli_query($conn, $sql);
 
 
-if($res==true){
+  if ($res == true) {
     print "<script>alert('Exclusão com sucesso');</script>";
     print "<script>location.href='?page=home';</script>";
-}else{
+  } else {
     print "<script>alert('Não foi possível excluir');</script>";
     print "<script>location.href='?page=home';</script>";
+  }
 }
-?>
